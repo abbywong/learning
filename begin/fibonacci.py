ce949f72c9ed6aaf0f373d72b088dfa50604a164
rafiki <mark@rafiki>
2016-04-19 20:57:48 +0200

    second challenge!

---
+++ begin/fibonacci.py
@@ -0,0 +1,13 @@
+
+"""
+The Fibonacci series is like this:
+
+    0 1 1 2 3 5 8 13 21 34 55 89 144 ...
+
+Can you see the pattern? Please complete the list below with at least 50 numbers (automatically).
+"""
+
+numbers = [0, 1, 1]
+
+
+
