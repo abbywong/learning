ac3cf0bd6a1ad012324fccb4b7d6d40982db8878
rafiki <mark@rafiki>
2016-04-19 21:01:49 +0200

    third challenge!

---
+++ begin/faculty.py
@@ -0,0 +1,36 @@
+
+"""
+Now you can learn to make a function. Here's an example:
+"""
+
+def square(x):
+	return x*x
+
+print('squares:')
+print(square(4))
+print(square(11))
+
+"""
+You added a new command to the language!
+
+Your function will be more difficult: you should calculate the faculty of a number:
+
+1! = 1
+2! = 2 * 1 = 2
+3! = 3 * 2 * 1 = 6
+4! = 4 * 3 * 2 * 1 = 24
+...
+8! = 8 * 7! = 40320
+
+Please change the function below to be correct:
+"""
+
+def faculty(number):
+	return number
+
+print('faculty:')
+print(faculty(4))
+print(faculty(6))
+print(faculty(9))
+
+
