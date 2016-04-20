cde296a6ca237123dee61839ad464ff97f86eb6a
abbywong <905418235@qq.com>
2016-04-20 18:15:24 +0800

    solved!

--- begin/cubes.py
+++ begin/cubes.py
@@ -4,5 +4,14 @@ Please store the third powers of all HALF numbers up to 1000 in a list, then pri
 
 [0**3, 0.5**3, 1**3, 1.5**3, 2**3, 2.5**3, ..., 999.5**3, 1000**3]
 """
+b=0
+cs=[]
+while b<=1000:
+	c=b**3
+	b=float(b+0.5)
+	cs.append(c)
+print(cs)
+
+
 
 
--- begin/pythagoras.py
+++ begin/pythagoras.py
@@ -1,5 +1,10 @@
 
+from math import sqrt
+
 """
 Make some code that calculates the long side of a right angle triangle (Pythagoras)
 """
-
+a=float(input('短边长a\n'))
+b=float(input('短边长b\n'))
+c=sqrt(a**2+b**2)
+print('长边长c',c)
