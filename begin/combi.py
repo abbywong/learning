afb4604b7d30a97ccf89009bca90bd88272a3d0f
rafiki <mark@rafiki>
2016-04-20 22:18:18 +0200

    easy

---
+++ begin/combi.py
@@ -0,0 +1,16 @@
+
+"""
+Several steps:
+
+1. Create a function `g` with 'argument' N that generates:
+
+	[1**2, 2**2, 3**2, ..., N**2]
+
+and gives back (`return`) the sum of this list.
+
+2. Use this function `g` to fill a list with the outcomes for EVEN numbers up to 150.
+"""
+
+def g(N):
+	return 0
+
